1e64d20b48e47ccec73280cddfc453fe19e62566
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 18:57:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	 const firstName = document.getElementById('form1').value;
+    const lastName = document.getElementById('form1').value;
+	alert("First Name: " + firstName + "\nLast Name: " + lastName);
 
 }
+document.getElementById('form1').addEventListener('submit', getFormvalue);
